c0e11d4409c0bfbda0dc6ae65a233b1c62ed4f76
Neo <lordneobarnachea@gmail.com>
2023-12-11 10:09:10 +0800

    added spring boot

---
+++ bookish-boulevard-backend/src/main/java/com/bookishboulevard/BookishBoulevardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookishboulevard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookishBoulevardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookishBoulevardBackendApplication.class, args);
+	}
+
+}
