a5df939e978736995a21f0e5812f87c01d090f86
Neo <lordneobarnachea@gmail.com>
2023-12-13 21:19:12 +0800

    added file structure

--- Bookish-Boulevard-Frontend/src/App.jsx
+++ Bookish-Boulevard-Frontend/src/App.jsx
@@ -1,32 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/');
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.text();
-        setMessage(data);
-
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    fetchData();
-  }, [])
 
   return (
     <>
-      <div>
+      <>
         <h1>this is a sample message</h1>
-        <h1>{message}</h1>
-      </div>
+      </>
     </>
   );
 }
