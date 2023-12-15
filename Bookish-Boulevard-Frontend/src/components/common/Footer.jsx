b02c2c3a93d949fbee9fbcc5e61a30c8a392e4af
Neo <lordneobarnachea@gmail.com>
2023-12-15 20:40:56 +0800

    change max width

--- Bookish-Boulevard-Frontend/src/components/common/Footer.jsx
+++ Bookish-Boulevard-Frontend/src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col md:flex-row h-[20vh] border border-red-500 items-center justify-center max-w-screen-xl mx-auto min-w-[310px]">
             Footer
-
+            
         </div>
     )
 }
--- Bookish-Boulevard-Frontend/src/components/common/Header.jsx
+++ Bookish-Boulevard-Frontend/src/components/common/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
 
-        <div className="flex flex-col md:flex-row h-[15vh] border border-red-500 items-center justify-center max-w-screen-xl mx-auto min-w-[310px]">
+        <div className="flex flex-col md:flex-row h-[15vh] border border-red-500 items-center justify-center max-w-screen-2xl mx-auto min-w-[310px]">
             <div className='flex flex-col md:flex-row w-full md:w-[65%]  justify-between md:h-[100%] items-center'>
                 <div className='pl-4 md:pl-8'>
                     <Link to="/">Bookish Boulevard</Link>
