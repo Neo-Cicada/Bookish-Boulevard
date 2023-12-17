93d1d7be5dbbea346b9461b1eda15710c1c96f72
Neo <lordneobarnachea@gmail.com>
2023-12-17 20:50:09 +0800

    fixing footer

--- Bookish-Boulevard-Frontend/src/components/common/Footer.jsx
+++ Bookish-Boulevard-Frontend/src/components/common/Footer.jsx
@@ -1,8 +1,10 @@
 
 export default function Footer() {
   return (
-    <div className="flex flex-col md:flex-row h-[20vh] border border-red-500 items-center justify-center max-w-screen-xl mx-auto min-w-[310px]">
-      Footer
+    <div className="">
+      <div>Logo</div>
+      <div>Company</div>
+      <div>Latest News</div>
     </div>
   );
 }
