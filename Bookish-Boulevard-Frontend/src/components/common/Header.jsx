24f370bcab7b1ff2afccdf8f79a82afe6d91e55f
Neo <lordneobarnachea@gmail.com>
2023-12-20 17:59:40 +0800

    working with burger navigation

--- Bookish-Boulevard-Frontend/src/components/common/Header.jsx
+++ Bookish-Boulevard-Frontend/src/components/common/Header.jsx
@@ -22,9 +22,9 @@ export default function Header() {
 
         <div className="flex flex-col md:flex-row h-[15vh] items-center justify-between max-w-screen-2xl mx-auto min-w-[370px] gap-2">
             <div className='flex flex-col sm:flex-row  justify-between items-center h-[100%] w-full ' >
-                <div className='h-[100%] pl-4 sm:w-80  md:pl-8 text-2xl flex  items-center sm:justify-center justify-between w-full ' >
+                <div className=' h-[100%] pl-4 sm:w-80  md:pl-8 text-2xl flex  items-center sm:justify-center justify-between w-full ' >
                     <Link to="/">
-                        <span className='font-extrabold'>Bookish</span> <i> Boulevard</i>
+                        <span className='font-extrabold hover:text-amber-900'>Bookish</span> <i> Boulevard</i>
                     </Link>
                     {isMobile && <BurgerNavigation />}
                 </div>
@@ -32,8 +32,9 @@ export default function Header() {
                 {!isMobile &&
                     <div className='w-[100%] sm:w[50%] h-[100%] flex justify-center items-center'> <Navigation /> </div>
                 }
+                
             </div>
-
+                
         </div>
 
     )
--- Bookish-Boulevard-Frontend/src/components/common/Navigation.jsx
+++ Bookish-Boulevard-Frontend/src/components/common/Navigation.jsx
@@ -3,25 +3,25 @@ export default function Navigation() {
     return (
         <>
             <ul className='flex w-full md:w-[75%] justify-between'>
-                <li className='cursor-pointer text-sm font-bold'>
+                <li className='cursor-pointer text-sm font-bold hover:text-amber-900'>
                     <NavLink to="/fiction"
                         style={({ isActive }) => (isActive ? { color: "#74642F", borderBottom: "2px solid #74642F" } : null)}
                     >
                         FICTION</NavLink>
                 </li>
-                <li className='cursor-pointer text-sm font-bold'>
+                <li className='cursor-pointer text-sm font-bold hover:text-amber-900'>
                     <NavLink
                         to="/nonFiction"
                         style={({ isActive }) => (isActive ? { color: "#74642F", borderBottom: "2px solid #74642F" } : null)}
 
                     > NON-FICTION</NavLink></li>
-                <li className='cursor-pointer text-sm font-bold'>
+                <li className='cursor-pointer text-sm font-bold hover:text-amber-900'>
                     <NavLink
                         to="/biographies"
                         style={({ isActive }) => (isActive ? { color: "#74642F", borderBottom: "2px solid #74642F" } : null)}
 
                     >BIOGRAPHIES</NavLink></li>
-                <li className='cursor-pointer text-sm font-bold'>
+                <li className='cursor-pointer text-sm font-bold hover:text-amber-900'>
                     <NavLink
                         style={({ isActive }) => (isActive ? { color: "#74642F", borderBottom: "2px solid #74642F" } : null)}
 
