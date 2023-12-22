4b15a2cf471be0c41f788d8cd3cf279b36c072b4
Neo <lordneobarnachea@gmail.com>
2023-12-22 20:16:56 +0800

    working with featured books

--- Bookish-Boulevard-Frontend/src/pages/landing/FeaturedBooks.jsx
+++ Bookish-Boulevard-Frontend/src/pages/landing/FeaturedBooks.jsx
@@ -2,7 +2,68 @@
 const FeaturedBooks = () => {
     return (
         <>
-            <div className="h-[100vh] max-h-[1000px] border border-red-500 max-w-screen-2xl m-auto min-w-[360px]">
+            <div className="h-[100vh] max-h-[1000px] border border-red-500 max-w-screen-2xl m-auto min-w-[360px] ">
+                <p className="h-[8%] text-center font-extrabold md:text-[47px] text-[30px]">Featured Books</p>
+                <div className="h-[92%] md:flex md:justify-around items-center ">
+                    <div className="h-1/4 md:h-[437px] border border-red-500 md:w-52 flex flex-row md:flex-col">
+
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[60%] flex items-center justify-center">
+                            <div className="border border-red-500 h-[95%] md:h-[90%] md:w-[90%]">
+                                BOOK COVER
+                            </div>
+                        </div>
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[40%]">
+                            <p>title</p>
+                            <p>Author</p>
+                            <p>Price $</p>
+                        </div>
+
+                    </div>
+                    <div className="h-1/4 md:h-[437px] border border-red-500 md:w-52 flex flex-row md:flex-col">
+
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[60%] flex items-center justify-center">
+                            <div className="border border-red-500 h-[95%] md:h-[90%] md:w-[90%]">
+                                BOOK COVER
+
+                            </div>
+                        </div>
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[40%]">
+                            <p>title</p>
+                            <p>Author</p>
+                            <p>Price $</p>
+                        </div>
+
+                    </div>
+                    <div className="h-1/4 md:h-[437px] border border-red-500 md:w-52 flex flex-row md:flex-col">
+
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[60%] flex items-center justify-center">
+                            <div className="border border-red-500 h-[95%] md:h-[90%] md:w-[90%]">
+                                BOOK COVER
+
+                            </div>
+                        </div>
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[40%]">
+                            <p>title</p>
+                            <p>Author</p>
+                            <p>Price $</p>
+                        </div>
+
+                    </div>
+                    <div className="h-1/4 md:h-[437px] border border-red-500 md:w-52 flex flex-row md:flex-col">
+
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[60%] flex items-center justify-center">
+                            <div className="border border-red-500 h-[95%] md:h-[90%] md:w-[90%]">
+                                BOOK COVER
+                            </div>
+                        </div>
+                        <div className="border border-red-500 w-[60%] md:w-full md:h-[40%]">
+                            <p>title</p>
+                            <p>Author</p>
+                            <p>Price $</p>
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
         </>
--- Bookish-Boulevard-Frontend/src/pages/landing/Landing.jsx
+++ Bookish-Boulevard-Frontend/src/pages/landing/Landing.jsx
@@ -14,18 +14,18 @@ export default function Landing() {
                     <p className="mx-10 text-center">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem minima modi pariatur. Odio iure dolorum modi at ut esse omnis maxime a, assumenda quo ipsam, quisquam nisi deserunt ipsum. Nisi?
                     </p>
-                    <button className="border border-gray-400 w-32   min-w-0 h-10">READ MORE</button>
+                    <button className="border border-gray-400 w-32 hover:border-none min-w-0 h-10 hover:bg-amber-800 hover:text-white">READ MORE</button>
                     <div></div>
                 </div>
                 <div className="w-full border border-red-500 h-[50%] flex items-center justify-center md:h-[100%]">
                     <div className="h-[80%] max-h-[500px] min-w-[250px] w-[40%] md:w-[50%] border border-red-500"></div>
                 </div>
             </div>
-            <FeaturedBooks/>
-            <BestSelling/>
-            <PopularBooks/>
-            <Quotes/>
-            <SaleBook/>
+            <FeaturedBooks />
+            <BestSelling />
+            <PopularBooks />
+            <Quotes />
+            <SaleBook />
         </>
     )
 }
