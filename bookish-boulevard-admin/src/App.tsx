9f7acbbebb1019aad88c9936e8b9f18afb66b6a0
Neo <lordneobarnachea@gmail.com>
2024-01-24 21:07:06 +0800

    add current time and date

--- bookish-boulevard-admin/src/App.tsx
+++ bookish-boulevard-admin/src/App.tsx
@@ -1,12 +1,26 @@
+import React, { useState, useEffect } from "react";
 import RouterPages from "./components/RouterPages"
 import Sidebar from "./components/Sidebar"
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <>
       <div className="flex">
         <Sidebar />
         <div className="w-full border border-red-500 h-100vh">
+          <div className="h-[10%] flex justify-between items-center">
+            <div>Name</div>
+            <div>{currentTime.toLocaleString()}</div>
+          </div>
           <RouterPages />
         </div>
 
