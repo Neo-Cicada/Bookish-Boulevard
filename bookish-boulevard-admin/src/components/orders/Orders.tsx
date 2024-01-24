15cc28ed02bb92e0586a04d31c80804e4ace3b25
Neo <lordneobarnachea@gmail.com>
2024-01-24 21:13:53 +0800

    fix height 100% -> 90%

--- bookish-boulevard-admin/src/components/orders/Orders.tsx
+++ bookish-boulevard-admin/src/components/orders/Orders.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Orders() {
   return (
-    <div className="w-[100%] bg-[#A7A7A7] h-full">
+    <div className="w-[100%] bg-[#A7A7A7] h-[90%]">
       Orders
     </div>
   )
--- bookish-boulevard-admin/src/components/settings/Settings.tsx
+++ bookish-boulevard-admin/src/components/settings/Settings.tsx
@@ -1,7 +1,7 @@
 
 export default function Settings() {
   return (
-    <div className="w-[100%] bg-[#A7A7A7]">
+    <div className="w-[100%] bg-[#A7A7A7] h-[90%]">
       Settings</div>
   )
 }
