91a5f88c470435b74db1a5f39cf8e47953a296d3
Neo <lordneobarnachea@gmail.com>
2024-01-30 21:37:43 +0800

    add order Box

---
+++ bookish-boulevard-admin/src/components/orders/OrderBox.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderBox(){
+    return(
+        <div className="bg-white w-[95%] h-[95%] rounded-md">   
+            
+        </div>
+    )
+}
--- bookish-boulevard-admin/src/components/orders/Orders.tsx
+++ bookish-boulevard-admin/src/components/orders/Orders.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import OrderBox from './OrderBox'
 export default function Orders() {
   return (
-    <div className="w-[100%] bg-[#A7A7A7] h-[90%]">
+    <div className="w-[100%] bg-[#A7A7A7] h-[90%] flex flex-col items-center">
       Orders
+      <OrderBox/>
     </div>
   )
 }
