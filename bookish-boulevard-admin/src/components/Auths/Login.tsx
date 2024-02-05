64b0e2fa72fa1852039159f2a6d97400b0d07ec7
Neo <lordneobarnachea@gmail.com>
2024-02-05 21:41:39 +0800

    Updated the login component

--- bookish-boulevard-admin/src/components/Auths/Login.tsx
+++ bookish-boulevard-admin/src/components/Auths/Login.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
     return (
         <div className="flex items-center justify-center w-screen h-screen border border-red-500">
+            <div className='border border-red-500 w-[40%] h-[80%] rounded-md flex flex-col justify-center'>
+                <form className='flex flex-col items-center justify-center'>
+                    <div>Logo</div>
+                    <div className='flex flex-col w-1/2'>
+                        <label htmlFor="email" className='text-md'>Email</label>
+                        <input
+                            placeholder='Email'
+                            id='email'
+                            name='email'
+                            className='border border-red-500' />
+                    </div>
+                    <div className='flex flex-col w-1/2'>
+                        <label htmlFor="email" className='text-md'>Password</label>
+                        <input
+                            placeholder='Email'
+                            id='email'
+                            name='email'
+                            className='border border-red-500' />
+                    </div>
 
-            <div className='w-[50%] h-[80%] rounded-md flex flex-col border border-red-500 justify-around'>
-                <div>Logo</div>
-                <form className="flex flex-col">
-                    <input type="text" name="email" id="email"/>
-                    <input type="passowrd" name="password" id="password"/>
-                    <button>Login</button>
                 </form>
             </div>
-
         </div>
-    )
+    );
 }
